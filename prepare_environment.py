091f7fb8911a6ec8e41cdac7dabfb107923b49c1
adamalloul <adampackets@gmail.com>
2017-02-28 22:35:57 +0100

    Added offerdata template and adapted the python script

--- prepare_environment.py
+++ prepare_environment.py
@@ -12,6 +12,11 @@ ip = "http://192.168.178.206:9200"
 print "Create logging template"
 infile = open("logging_template.json","r")
 content = json.dumps(json.loads(infile.read()))
+
+r = requests.delete(ip+"/_template/logging_template")
+if (r.status_code > 300):
+    print "delete status is %s, reason is %s" % (r.status_code, r.reason)
+
 r = requests.put(ip+"/_template/logging_template", data=content)
 if (r.status_code > 300):
     print "status is %s, reason is %s" % (r.status_code, r.reason)
@@ -19,10 +24,30 @@ else:
     print "  -> OK"
 infile.close()
 
+
 print "Create userbase template"
-infile = open("userbase_template.json", "r")
+r = requests.delete(ip+"/_template/userbase_template")
+if (r.status_code > 300):
+    print "delete status is %s, reason is %s" % (r.status_code, r.reason)
+
+infile = open("template_userbase.json", "r")
 content = json.dumps(json.loads(infile.read()))
 r = requests.put(ip+"/_template/userbase_template", data=content)
+if (r.status_code > 300):
+    print "status is %s, reason is %s" % (r.status_code, r.reason)
+else:
+    print "  -> OK"
+
+infile.close()
+
+print "Create offerdata template"
+r = requests.delete(ip+"/_template/offerdata_userbase")
+if (r.status_code > 300):
+    print "delete status is %s, reason is %s" % (r.status_code, r.reason)
+
+infile = open("template_offerdata.json", "r")
+content = json.dumps(json.loads(infile.read()))
+r = requests.put(ip+"/_template/offerdata_userbase", data=content)
 if (r.status_code > 300):
     print "status is %s, reason is %s" % (r.status_code, r.reason)
 else:
